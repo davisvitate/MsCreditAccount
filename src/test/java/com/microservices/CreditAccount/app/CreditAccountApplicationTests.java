d23c79df3ebc333153e24371e459bf6e79a12e70
davisvitate <davisvitate@gmail.com>
2019-12-09 15:26:48 -0500

    first commit

---
+++ src/test/java/com/microservices/CreditAccount/app/CreditAccountApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservices.CreditAccount.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreditAccountApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
